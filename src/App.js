2d40e190b3de9b6b2b929e57b73a09e516ee337c
mmdanish <mhddanish567@gmail.com>
2024-09-16 12:18:09 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Analytics from "./components/Analytics";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+
+
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Analytics />
+    </div>
+  );
+}
+
+export default App;
